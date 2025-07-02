4d8f668f37f1248acb4971bb6c92a300560d1052
Daniel Kesselberg <mail@danielkesselberg.de>
2025-07-02 17:27:38 +0200

    feat: omit an option by throwing an event
    The mail app uses NcSelect for selecting email recipients, so we would like to validate the input in the createOption callback to ensure only valid email addresses are added as options.
    
    Signed-off-by: Daniel Kesselberg <mail@danielkesselberg.de>

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -553,6 +553,7 @@ export default {
     /**
      * User defined function for adding Options
      * @type {Function}
+     * @throws {Error} throw error to omit an option
      */
     createOption: {
       type: Function,
@@ -974,9 +975,13 @@ export default {
         ? this.filter(optionList, this.search, this)
         : optionList
       if (this.taggable && this.search.length) {
-        const createdOption = this.createOption(this.search)
-        if (!this.optionExists(createdOption)) {
-          options.unshift(createdOption)
+        try {
+          const createdOption = this.createOption(this.search)
+          if (!this.optionExists(createdOption)) {
+            options.unshift(createdOption)
+          }
+        } catch (e) {
+          // omit option on error
         }
       }
       return limitOptions(options)
